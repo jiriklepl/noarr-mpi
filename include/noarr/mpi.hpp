3aa7521a41bd353b5fa808618ea347dca260c723
Jiří Klepl <klepl@d3s.mff.cuni.cz>
2025-02-06 22:47:36 +0100

    add noarr/mpi.hpp

---
+++ include/noarr/mpi.hpp
@@ -0,0 +1,16 @@
+/**
+ * @file mpi.hpp
+ * @brief This file includes all the headers of the Noarr MPI library.
+ *
+ */
+#ifndef NOARR_MPI_HPP
+#define NOARR_MPI_HPP
+
+#include "noarr/structures/interop/mpi_algorithms.hpp"
+#include "noarr/structures/interop/mpi_bag.hpp"
+#include "noarr/structures/interop/mpi_structs.hpp"
+#include "noarr/structures/interop/mpi_transform.hpp"
+#include "noarr/structures/interop/mpi_traverser.hpp"
+#include "noarr/structures/interop/mpi_utility.hpp"
+
+#endif // NOARR_MPI_HPP
