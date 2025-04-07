8737aedf8a04b8708be8f36e0136197e5e7a1389
Jiří Klepl <klepl@d3s.mff.cuni.cz>
2025-04-07 12:29:43 +0200

    add kokkos

--- configure.sh
+++ configure.sh
@@ -3,6 +3,8 @@
 set -euo pipefail
 
 BUILD_DIR=${BUILD_DIR:-build}
+NUM_JOBS=${NUM_JOBS:-$(nproc)}
+
 
 BUILD_DIR=$(realpath "${BUILD_DIR}")
 
@@ -10,16 +12,28 @@ CONFIG=${1:-Release}
 
 git submodule update --init --recursive
 
+# Configure Kokkos
+cmake -B "${BUILD_DIR}/kokkos-build" -S vendor/kokkos \
+	-D CMAKE_BUILD_TYPE="${CONFIG}" \
+	-D CMAKE_INSTALL_PREFIX="${BUILD_DIR}/kokkos-install" \
+	-D CMAKE_EXPORT_COMPILE_COMMANDS=ON
+
+# Build and install Kokkos
+cmake --build "${BUILD_DIR}/kokkos-build" --target install --config "${CONFIG}" --parallel "${NUM_JOBS}"
+
+CMAKE_PREFIX_PATH="${BUILD_DIR}/kokkos-install${CMAKE_PREFIX_PATH:+;$CMAKE_PREFIX_PATH}"
+
 # Configure Kokkos-comm
 cmake -B "${BUILD_DIR}/kokkos-comm-build" -S vendor/kokkos-comm \
 	-D CMAKE_BUILD_TYPE="${CONFIG}" \
+	-D CMAKE_PREFIX_PATH="${CMAKE_PREFIX_PATH}" \
 	-D CMAKE_INSTALL_PREFIX="${BUILD_DIR}/kokkos-comm-install" \
 	-D CMAKE_EXPORT_COMPILE_COMMANDS=ON
 
 # Build and install Kokkos-comm
-cmake --build "${BUILD_DIR}/kokkos-comm-build" --target install --config "${CONFIG}" --parallel
+cmake --build "${BUILD_DIR}/kokkos-comm-build" --target install --config "${CONFIG}" --parallel "${NUM_JOBS}"
 
-CMAKE_PREFIX_PATH="${BUILD_DIR}/kokkos-comm-install${CMAKE_PREFIX_PATH:+:$CMAKE_PREFIX_PATH}"
+CMAKE_PREFIX_PATH="${BUILD_DIR}/kokkos-comm-install${CMAKE_PREFIX_PATH:+;$CMAKE_PREFIX_PATH}"
 
 cmake -B "${BUILD_DIR}" -S . \
 	-D CMAKE_BUILD_TYPE="${CONFIG}" \
